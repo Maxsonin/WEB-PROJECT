71435cc75977b58d712558cbac15681c716f3ae9
Maxsonin <maximles.lv@gmail.com>
2025-03-11 13:35:10 +0200

    Initial commit for Lab5

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api')
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <>
+      <p>{message}</p>
+    </>
+  );
+}
+
+export default App;
